78e85483fdcb50b7d31a3fabad9379d29987d7cd
Vikas <2003vikas0906@gmail.com>
2024-08-10 10:38:32 +0530

    templates added

--- template.py
+++ template.py
@@ -29,3 +29,21 @@ list_of_files = [
 ]
 
 #basically all the things we need are in template files
+
+
+# making directories of dependencies if not exists 
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    # splits file and folder 
+    filedir,filename = os.path.split(filepath)
+
+    if filedir != "":
+        # to create the file 
+        os.makedirs(filedir,exist_ok= True)
+        logging.info(f"Created directory: {filedir} for the file {filename}")
+    
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,'w') as f:
+            logging.info(f"Created file: {filepath}")
+    else:
+        logging.info(f"File {filepath} already exists.")
